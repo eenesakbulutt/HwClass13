cca229e686f0f1b82dba3d2c04c172a856c05100
Enes Akbulut <eenesakbulutt@gmail.com>
2021-08-21 02:00:21 -0400

    StudentObj

---
+++ Student.java
@@ -0,0 +1,28 @@
+package com.syntax.class13;
+
+public class Student {
+	
+	
+	String name;
+	String id;
+	char gender;
+	double tutition;
+	String school;
+	
+	void study() {
+		
+	System.out.println(name+" studies in "+ school);	
+			
+	}
+	
+	
+	void payTuition() {
+		
+		System.out.println(name+" pay "+tutition);
+	}
+	
+	
+	
+	
+
+}
---
+++ StudentTester.java
@@ -0,0 +1,36 @@
+package com.syntax.class13;
+
+public class StudentTester {
+
+	public static void main(String[] args) {
+		
+		
+	Student Michael= new Student();
+	Michael.name="Michael";
+	Michael.id="123";
+	Michael.gender='M';
+	Michael.school="Syntax";
+	Michael.tutition=1000;
+	Michael.study();
+	Michael.payTuition();
+		
+		
+		
+	Student Anne= new Student();
+	Anne.name="Anne Mary";
+	Anne.school="Syntax";
+	Anne.tutition=1400;
+	Anne.study();
+	Anne.payTuition();
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
